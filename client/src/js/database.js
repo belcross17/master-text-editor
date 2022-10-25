11c98ad9369d25abd0dcafe91f71e5162b120f05
Brayan <alfredochabes1@hotmail.com>
2022-10-25 10:55:30 -0400

    2nd attempt to fix jate header not displaying

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,11 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async (value) => {
+export const getDb = async () => {
   console.log('Get data from the jateDB')
 
   const jateDB = await openDB('jate', 1);
-  const tx = jateDB.transaction('jate', 'readonly');
+  const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
@@ -26,13 +26,13 @@ export const getDb = async (value) => {
 }
 9
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, value) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
 
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: id, value: value});
+  const request = store.put({content});
   const result = await request;
   console.log('Data saved to the DB', result);
 }
